6c2f49368ad40762872a207885e2590c21c70b8e
nitinsriv <snitin@sconcesolutions.com>
2017-04-25 14:21:56 +0200

    config parametrization
    avoiding hardcoding properties

---
+++ experiment/config_deployment/R/popularity.R
@@ -0,0 +1,16 @@
+
+#read config file from working directory
+config_file <-  read.csv("config.csv",sep="=",stringsAsFactors = F)
+
+#read value of root_dir
+base_dir <- config_file$value[1]
+
+#initialize data folder and data file
+data_dir <- "data/"
+data_file <- "popularity.csv"
+
+#construct data file path from config file
+abs_datafile_path <- paste(base_dir,data_dir,data_file, sep="")
+
+#read data file
+popularity_data <- read.csv(abs_datafile_path, stringsAsFactors = F)
